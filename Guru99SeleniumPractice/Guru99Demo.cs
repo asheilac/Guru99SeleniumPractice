882d19451ce1be6ff9ab81b25d34f3e10779ed3e
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-10-21 13:39:39 +0100

    selenium first test

---
+++ Guru99SeleniumPractice/Guru99Demo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Guru99SeleniumPractice
+{
+    public class Guru99Demo
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void StartBrowser()
+        {
+            driver = new ChromeDriver("C:\\Tools");
+        }
+
+        [Test]
+        public void Test()
+        {
+            driver.Url = "http://www.google.co.in";
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            driver.Close();
+        }
+    }
+}
