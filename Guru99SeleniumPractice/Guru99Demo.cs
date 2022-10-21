5cbe09ffc2fdcc353b8629f89ce29d60b822f24a
Sheila Coles <sheila.coles@ukho.gov.uk>
2022-10-21 13:52:51 +0100

    test guru99 testing linkfails due to pop-ups intercepting

--- Guru99SeleniumPractice/Guru99Demo.cs
+++ Guru99SeleniumPractice/Guru99Demo.cs
@@ -25,6 +25,20 @@ namespace Guru99SeleniumPractice
             driver.Url = "http://www.google.co.in";
         }
 
+        [Test]
+        public void TestCanGoToGuru99AndClickTesting()
+        {
+            driver.Url = "http://demo.guru99.com/test/guru99home/";
+
+            IWebElement testingElement =
+                driver.FindElement(By.XPath("//*[@id=\"rt-header\"]/div/div[2]/div/ul/li[2]/a"));
+            testingElement.Click();
+            string title = driver.Title;
+
+            Assert.That(title, Is.EqualTo("Software Testing Tutorial"));
+        }
+
+
         [TearDown]
         public void CloseBrowser()
         {
